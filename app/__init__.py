97ceb4a1cd8e0f170d9cd1061057d86300f7c7e9
RB <rbajaj@redhat.com>
2015-09-30 09:03:52 +0530

    Minor setup updates

--- app/__init__.py
+++ app/__init__.py
@@ -7,19 +7,19 @@ Created on Fri Jul 24 16:34:28 2015
 
 from flask import Flask
 #from flask.ext.sqlalchemy import SQLAlchemy
-from flaskext.mysql import MySQL
+#from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
 #db = SQLAlchemy(app)
 
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'rbajaj'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'nxzd8978'
-app.config['MYSQL_DATABASE_DB'] = 'rhpartners'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+#
+#mysql = MySQL()
+#app.config['MYSQL_DATABASE_USER'] = 'rbajaj'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'nxzd8978'
+#app.config['MYSQL_DATABASE_DB'] = 'rhpartners'
+#app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
 
 from app import views

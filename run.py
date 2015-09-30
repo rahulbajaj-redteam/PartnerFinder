c79075456bc7bed074a9f5da058abbdf4b72bf71
RB <rbajaj@redhat.com>
2015-09-30 09:02:23 +0530

    run

--- run.py
+++ run.py
@@ -7,7 +7,9 @@ Created on Fri Jul 24 16:53:59 2015
 
 #!flask/bin/python
 from app import app
-app.run(host= '10.65.161.196',debug=True)
+app.run(host= '',debug=True)
+
+#app.run(host= '10.65.161.196',debug=True)
 #app.run(host= '10.65.165.225',debug=True)
 #app.run(host= '10.36.5.174',debug=True)
 #app.run(host= '192.168.1.11',debug=True)
